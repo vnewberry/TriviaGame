bc817835eccc2cc0ec50f86e64fd9edd4f8299f2
Vance Newberry <vancenewberry@gmail.com>
2020-03-11 22:57:35 -0500

    randomness added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,7 @@ function gameSet() {
   onQuestion = 0;
   right = 0;
   wrong = 0;
+  popArray();
   $("#slot").empty();
   $("#slot").append(
     "<h1 id ='start-instructions'>Click the button to begin</h1>"
@@ -23,62 +24,95 @@ function gameSet() {
 var right = 0;
 var wrong = 0;
 var onQuestion = 0;
+var questionSet
 var question;
-var questionSet = [
+function popArray(){
+  questionSet = [
   (question = {
-    q: "what is the answer to question 1?",
-    w1: "wrong answer 11",
-    w2: "wrong answer 12",
-    w3: "wrong answer 13",
-    r1: "right answer1"
+    q: "Roger Bannister was the first person to do what?",
+    w1: "Walk on the moon.",
+    w2: "Be cured of old Age.",
+    w3: "Win the Super Bowl, Stanley Cup, and the Daytona 500",
+    r1: "Run a mile in less than 4 minutes",
+    
   }),
   (question = {
-    q: "what is the answer to question 2?",
-    w1: "wrong answer 21",
-    w2: "wrong answer 22",
+    q: "The short blade that Bilbo Baggins used and passed on to Frodo was called?",
+    w1: "Orcrist",
+    w2: "Butter Knife",
     w3: "wrong answer 23",
     r1: "right answer2"
   }),
   (question = {
-    q: "what is the answer to question 3?",
+    q: "3?",
     w1: "wrong answer 31",
     w2: "wrong answer 32",
     w3: "wrong answer 33",
     r1: "right answer3"
   }),
   (question = {
-    q: "what is the answer to question 4?",
+    q: "4?",
     w1: "wrong answer 41",
     w2: "wrong answer 42",
     w3: "wrong answer 43",
     r1: "right answer4"
   }),
   (question = {
-    q: "what is the answer to question 5?",
+    q: "5?",
     w1: "wrong answer 51",
     w2: "wrong answer 52",
     w3: "wrong answer 53",
     r1: "right answer5"
   })
 ];
-
+}
 function renderQuestion(n) {
   $("#slot").empty();
+  
+  
+  if (n < 5) {
+    //randomly generate the question that is rendered
+    var thisQuestion = (Math.floor(Math.random() * (questionSet.length)));
+    console.log("rand: "+thisQuestion);
+  $("#slot").append(`<h1>${questionSet[thisQuestion].q}</h1>`);
+  
+  var ans1 =$(
+    `<button class = 'answer wrong'>${questionSet[thisQuestion].w1}</button>`
+  );
+  var ans2 =$(
+    `<button class = 'answer wrong'>${questionSet[thisQuestion].w2}</button>`
+  );
+  var ans3 =$(
+    `<button class = 'answer wrong'>${questionSet[thisQuestion].w3}</button>`
+  );
+  var ans4 =$(
+    `<button class = 'answer right'>${questionSet[thisQuestion].r1}</button>`
+  );
+  var answers = [ans1,ans2,ans3,ans4];
+  
+  for(i=0;i<4;i++){
+    var ansIndex = (Math.floor(Math.random() * (answers.length)));
+    $("#slot").append(answers[ansIndex]);
+    answers.splice(ansIndex,1);
+  }
 
-  if (n < questionSet.length) {
-    $("#slot").append(`<h1>${questionSet[n].q}</h1>`);
-    $("#slot").append(
-      `<button class = 'answer wrong'>${questionSet[n].w1}</button>`
-    );
-    $("#slot").append(
-      `<button class = 'answer wrong'>${questionSet[n].w2}</button>`
-    );
-    $("#slot").append(
-      `<button class = 'answer wrong'>${questionSet[n].w3}</button>`
-    );
-    $("#slot").append(
-      `<button class = 'answer right'>${questionSet[n].r1}</button>`
-    );
+  questionSet.splice(thisQuestion,1);
+    // $("#slot").append(`<h1>${questionSet[thisQuestion].q}</h1>`);
+    // $("#slot").append(
+    //   `<button class = 'answer wrong'>${questionSet[thisQuestion].w1}</button>`
+    // );
+    // $("#slot").append(
+    //   `<button class = 'answer wrong'>${questionSet[thisQuestion].w2}</button>`
+    // );
+    // $("#slot").append(
+    //   `<button class = 'answer wrong'>${questionSet[thisQuestion].w3}</button>`
+    // );
+    // $("#slot").append(
+    //   `<button class = 'answer right'>${questionSet[thisQuestion].r1}</button>`
+    // );
+    // questionSet.splice(thisQuestion,1);
+
+    //   console.log(questionSet);
   } else {
     endGame();
   }
